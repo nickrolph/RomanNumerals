1a2241a90677e2d3feb882ab92c93a707dd9df42
Nicholas <nick@DESKTOP-9I7L2IS.localdomain>
2018-04-26 23:39:04 -0700

    test 4 complete, displays multiples pretty well see inline comments for details

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 function decToRome(x) {
+    romNum = ""
 	if(x >= 1000){
-		return "M"
+        //for loop runs as many times as 1000 divides x
+        for(var i = 0; i < Math.floor((x/1000)); i++){
+            //append a new M for each loop
+            romNum = romNum + "M"
+        }
+		return romNum
 	}
 	if(x >= 500){
 		return "D"
 	}
 	if(x >= 100){
-		return "C"
+        //for loop runs as many times as 100 divides x
+        for(var i = 0; i < Math.floor((x/100)); i++){
+            //append C for each loop
+            romNum = romNum + "C"
+
+        }
+		return romNum
 	}
 	if(x >= 50){
 		return "L"
 	}
 	if(x >= 10){
-		return "X"
+        //for loopo runs as many times as 10 divides x
+        for(var i = 0; i < Math.floor((x/10)); i++){
+            //append X for each loop
+            romNum = romNum + "X"
+        }
+		return romNum
 	}
 	if(x >= 5){
 		return "V"
 	}
 	if(x >= 1){
-		return "I"
+        //for loop runs as many times as 1 divides x
+        for(var i = 0; i < Math.floor((x/1)); i++){
+            //append I for each loop
+            romNum = romNum + "I"
+        }
+		return romNum
 	}
 
 return "Not a Valid input"
--- index.spec.js
+++ index.spec.js
@@ -16,26 +16,54 @@ describe('', () => {
 	equal(decToRome(50),"L")}) 
     it('', () => { // Test if 100 return C
 	equal(decToRome(100),"C")}) 
-    it('', () => { // Test if 500 return D
+	it('', () => { // Test if 500 return D
 	equal(decToRome(500),"D")}) 
-    it('', () => { // Test if 1000 return M
+ 	it('', () => { // Test if 1000 return M
 	equal(decToRome(1000),"M")}) 
 
-//test wave 3 to return nearest roman numeral below numerical value
+//test wave 3 to return nearest roman numeral below dec value
 	it('', () => { // Test if 1000 return M
 	equal(decToRome(1000),"M")})
-	it('', () => { // Test if 1000 return M
-	equal(decToRome(3000),"M")}) 
-	it('', () => { // Test if 1000 return M
-	equal(decToRome(200),"C")}) 
-	it('', () => { // Test if 1000 return M
+	it('', () => { // Test if 3000 return M - now MMM
+	equal(decToRome(3000),"MMM")}) 
+	it('', () => { // Test if 200 return C - now CC
+	equal(decToRome(200),"CC")}) 
+	it('', () => { // Test if 540 return D
 	equal(decToRome(540),"D")}) 
-	it('', () => { // Test if 1000 return M
+	it('', () => { // Test if 9 return V
 	equal(decToRome(9),"V")}) 
-	it('', () => { // Test if 1000 return M
-	equal(decToRome(4),"I")}) 
+	it('', () => { // Test if 4 return I - now IIII
+	equal(decToRome(4),"IIII")}) 
+
+//test wave 4 returns Roman Numeral n times wher n = the max times 
+//nearest Roman Numeral value below divides dec number
+	it('', () => { // Test if 3000 return MMM
+	equal(decToRome(3000),"MMM")}) 
+
+	it('', () => { // Test if 2000 return MM
+	equal(decToRome(2000),"MM")}) 
+
+	it('', () => { // Test if 30 returns correct
+	equal(decToRome(30),"XXX")})
+
+    it('', () => { // Test if 200 return CC
+	equal(decToRome(200),"CC")}) 
+    
+    it('', () => { // Test if 40 returns XXXX
+	equal(decToRome(40),"XXXX")}) 
+
+    it('', () => { // Test if 3 return III
+	equal(decToRome(3),"III")}) 
+
+    it('', () => { // Test if 8 return M
+	equal(decToRome(8),"V")}) 
 
 
+
+
+
+
+ 
 
 
 
