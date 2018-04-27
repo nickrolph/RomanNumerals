4ab4137e202b613c69811a1e6f17ada3408bd1e1
Nicholas <nick@DESKTOP-9I7L2IS.localdomain>
2018-04-27 16:02:19 -0700

    fixed problem with subtraction of X happening outside for loop

--- index.js
+++ index.js
@@ -30,42 +30,18 @@ function romToDec(x){
     var posNum = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
     
     while(x.length > 0){
- /*
-        //check the conents of the first letter and compare to second letter
-        firstLet = x.substring(0,1)
-        secLet = x.substring(1,2)
-        lets = ""
-
-        //this series of if's and else's checks if the first letter and second
-        //letter are a special case that has a different value
-        if((firstLet == "I" && (secLet == "V" || secLet == "X"))
-            ||(firstLet == "X" && (secLet == "L" || secLet == "C"))
-            ||(firstLet == "C" && (secLet == "D" || secLet == "M"))
-            ){
-            lets = x.substring(0,2)
-        }
-        else{
-            lets = firstLet
-        }
-*/
         //this loop checks each possible Numeral and adds the value based
         //off the first letter(s)
         for(var i = 0;i <posNum.length;i++){
             //checks if posNum[i] is in x and if it is at the begenning of
             //the string
-            console.log("WELL x is ", x)
-
-            console.log("The value of the loop is "+ i + " and the value of posNum[i] is ", posNum[i])
-            console.log("Hey there champ the value of the indexOf is ", x.indexOf(posNum[i]))
-
             if(x.indexOf(posNum[i]) === 0){
                 //if it is we add the decimal value
                 decVal = decVal + posDec[i]
+                //we then make x the same string without that first numeral(s)
+                x = x.substring(posNum[i].length,)
             }
-        console.log("WELL x is ", x)
-        //we then make x the same string without that first numeral(s)
-        x = x.substring(posNum[i].length,)
-        } 
+       } 
     }
     return decVal
 }
