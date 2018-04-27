5cfb7edaa7462d5e10c0c2cd5befb2bf7ca24884
Nicholas <nick@DESKTOP-9I7L2IS.localdomain>
2018-04-26 22:39:20 -0700

    Test wave 3. Checked for returning closest Roman numeral below given number

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 function decToRome(x) {
-	if(x === 1){
-		return "I"
-	}
-	if(x === 5){
-		return "V"
-	}
-	if(x === 10){
-		return "X"
-	}
-	if(x === 50){
-		return "L"
-	}
-	if(x === 100){
-		return "C"
-	}
-	if(x === 500){
-		return "D"
-	}
-	if(x === 1000){
+	if(x >= 1000){
 		return "M"
 	}
+	if(x >= 500){
+		return "D"
+	}
+	if(x >= 100){
+		return "C"
+	}
+	if(x >= 50){
+		return "L"
+	}
+	if(x >= 10){
+		return "X"
+	}
+	if(x >= 5){
+		return "V"
+	}
+	if(x >= 1){
+		return "I"
+	}
 
-
-
-
-
-
-
+return "Not a Valid input"
 
 
 }
--- index.spec.js
+++ index.spec.js
@@ -2,9 +2,13 @@ const { decToRome } = require('./index')
 const equal = require('assert').deepEqual
 
 describe('', () => {
+
+//test wave 1 for 1
     it('', () => { // Test if 1 return I
 	equal(decToRome(1),"I")}) 
-    it('', () => { // Test if 5 return V
+    
+//test wave 2 for returns of exact Roman Numerals
+	it('', () => { // Test if 5 return V
 	equal(decToRome(5),"V")}) 
     it('', () => { // Test if 10 return X
 	equal(decToRome(10),"X")}) 
@@ -16,4 +20,27 @@ describe('', () => {
 	equal(decToRome(500),"D")}) 
     it('', () => { // Test if 1000 return M
 	equal(decToRome(1000),"M")}) 
+
+//test wave 3 to return nearest roman numeral below numerical value
+	it('', () => { // Test if 1000 return M
+	equal(decToRome(1000),"M")})
+	it('', () => { // Test if 1000 return M
+	equal(decToRome(3000),"M")}) 
+	it('', () => { // Test if 1000 return M
+	equal(decToRome(200),"C")}) 
+	it('', () => { // Test if 1000 return M
+	equal(decToRome(540),"D")}) 
+	it('', () => { // Test if 1000 return M
+	equal(decToRome(9),"V")}) 
+	it('', () => { // Test if 1000 return M
+	equal(decToRome(4),"I")}) 
+
+
+
+
+
+
+ 
+
+
 })
