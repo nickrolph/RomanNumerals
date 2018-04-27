52f3ca522a780796584686a97a5bec51a9d8503a
Nicholas <nick@DESKTOP-9I7L2IS.localdomain>
2018-04-26 21:29:06 -0700

    Second wave Tests for exact Roman numeral Values

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 function decToRome(x) {
-    return "I"
+	if(x === 1){
+		return "I"
+	}
+	if(x === 5){
+		return "V"
+	}
+	if(x === 10){
+		return "X"
+	}
+	if(x === 50){
+		return "L"
+	}
+	if(x === 100){
+		return "C"
+	}
+	if(x === 500){
+		return "D"
+	}
+	if(x === 1000){
+		return "M"
+	}
+
+
+
+
+
+
+
+
+
 }
 
 module.exports = {
--- index.spec.js
+++ index.spec.js
@@ -4,4 +4,16 @@ const equal = require('assert').deepEqual
 describe('', () => {
     it('', () => { // Test if 1 return I
 	equal(decToRome(1),"I")}) 
+    it('', () => { // Test if 5 return V
+	equal(decToRome(5),"V")}) 
+    it('', () => { // Test if 10 return X
+	equal(decToRome(10),"X")}) 
+    it('', () => { // Test if 50 return L
+	equal(decToRome(50),"L")}) 
+    it('', () => { // Test if 100 return C
+	equal(decToRome(100),"C")}) 
+    it('', () => { // Test if 500 return D
+	equal(decToRome(500),"D")}) 
+    it('', () => { // Test if 1000 return M
+	equal(decToRome(1000),"M")}) 
 })
